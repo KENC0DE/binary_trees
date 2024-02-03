daba303064b4987aacd69752c09923fddcbdf5d0
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-03 21:23:19 +0100

    test

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,21 +2,22 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling
+ * @node: a binary tree node
  *
- * Return: pointer to sibling node.
-*/
+ * Return: the sibling of a node
+ */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent;
 
 	if (!node || !node->parent)
 		return (NULL);
+
 	parent = node->parent;
+
 	if (!parent->left || !parent->right)
 		return (NULL);
 
-	if (parent->left->n == node->n)
-		return (parent->right);
-	return (parent->left);
+	return (parent->left == node ? parent->right : parent->left);
 }
